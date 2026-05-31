5772dd5622cfc1a5b84c4a95046f1d9d3b8c72bd
sandeeprkulkarni <sandeep.kulkarni0@gmail.com>
2026-05-31 17:54:25 +0200

    Commited code to make it sure that correct file will be replaced

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,4 +1,3 @@
-// frontend/src/routes.tsx
 import { createBrowserRouter } from "react-router";
 import { RootLayout } from "./components/RootLayout";
 import { Dashboard } from "./components/Dashboard";
@@ -8,7 +7,7 @@ import { LocationGallery } from "./components/LocationGallery";
 import { EventGallery } from "./components/EventGallery";
 import { UnidentifiedPhotos } from "./components/UnidentifiedPhotos";
 import { Settings } from "./components/Settings";
-import { Deduplicator } from "./components/Deduplicator"; // Imported Page Element
+import { Deduplicator } from "./components/Deduplicator"; 
 
 export const router = createBrowserRouter([
   {
@@ -18,11 +17,19 @@ export const router = createBrowserRouter([
       { index: true, Component: Dashboard },
       { path: "train", Component: FaceTraining },
       { path: "organize", Component: PhotoOrganization },
-      { path: "deduplicate", Component: Deduplicator }, // Registered Component Path
+      { path: "deduplicate", Component: Deduplicator }, 
       { path: "locations", Component: LocationGallery },
       { path: "events", Component: EventGallery },
       { path: "unidentified", Component: UnidentifiedPhotos },
       { path: "settings", Component: Settings },
     ],
   },
-]);
+], {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  }
+});
